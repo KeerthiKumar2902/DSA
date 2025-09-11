9626668058c37b6347c2cc0974e1cf757ed9f4dc
KeerthiKumar2902 <keerthikumarkk1@gmail.com>
2025-09-11 23:09:47 +0530

    Time: 2 ms (77.03%) | Memory: 44.7 MB (73.59%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map=new HashMap<>();
+        int array[]=new int[26];
 
-        for(char c: magazine.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
+        for(char c:magazine.toCharArray()){
+            array[c-'a']++;
         }
 
         for(char c: ransomNote.toCharArray()){
-            if(map.containsKey(c)){
-                map.put(c,map.get(c)-1);
-                if(map.get(c)==0){
-                    map.remove(c);
-                }
+            if(array[c-'a']>0){
+                array[c-'a']--;
             }
             else{
                 return false;
             }
         }
+
         return true;
     }
 }
