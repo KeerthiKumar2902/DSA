c497f20247fb1cd28d7f7a3b4d040cc8f5663939
KeerthiKumar2902 <keerthikumarkk1@gmail.com>
2026-02-11 15:27:26 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (95.09%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -1,56 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
     public TreeNode sortedListToBST(ListNode head) {
-        
-        List<Integer> list=new ArrayList<>();
-        while(head!=null){
-            list.add(head.val);
-            head=head.next;
+        if(head == null) return null;
+        if(head.next == null) return new TreeNode(head.val);
+
+        ListNode slow = head, fast = head, prev = null;
+
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        int nums[]=new int[list.size()];
+        prev.next = null; // split list
 
-        for(int i=0;i<list.size();i++){
-            nums[i]=list.get(i);
-        }
+        TreeNode root = new TreeNode(slow.val);// removed the left part or LS and right part of LS seaprately both are individual list now ... and the middle node which was in slow is a new TreeNode which acts as a root
 
-        return arrayToBST(nums,0,nums.length-1);
-    }
-    public static TreeNode arrayToBST(int nums[],int start,int end){
-        if(start>end){
-            return null;
-        }
+        root.left  = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
 
-        int mid=(start+end)/2;
-        
-        TreeNode root=new TreeNode(nums[mid]);
-
-        root.left=arrayToBST(nums, start, mid-1);
-        root.right=arrayToBST(nums, mid+1, end);
         return root;
     }
 }
