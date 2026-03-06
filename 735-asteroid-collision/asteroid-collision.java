d96cb14272f570841c05322865959bd02b6f011c
KeerthiKumar2902 <keerthikumarkk1@gmail.com>
2026-03-06 22:34:52 +0530

    Time: 5 ms (65.93%) | Memory: 47.2 MB (29.26%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -2,14 +2,17 @@ class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack=new Stack<>();
 
-        for(int i: asteroids){
-            if(i>0) stack.push(i);
-
+        for(int i:asteroids){
+            if(i>0){
+                stack.push(i);
+            }
             else{
-                while(!stack.isEmpty() && stack.peek()>0 && stack.peek()<Math.abs(i))
+                while(!stack.isEmpty() && stack.peek()>0 && stack.peek()<Math.abs(i)){
                     stack.pop();
+                }
 
-                if(!stack.isEmpty() && Math.abs(i)==stack.peek()){
+
+                if(!stack.isEmpty() && stack.peek()==Math.abs(i)){
                     stack.pop();
                 }
                 else if(stack.isEmpty() || stack.peek()<0){
@@ -19,12 +22,12 @@ class Solution {
         }
 
         int n=stack.size();
-        int out[]=new int[n];
-        for(int i=0;i<n;i++){
-            int a=stack.pop();
-            out[n-i-1]=a;
-        }
+        int result[]=new int[n];
 
-        return out;
+
+        for(int i=0;i<result.length;i++){
+            result[n-i-1]=stack.pop();
+        }
+        return result;
     }
 }
